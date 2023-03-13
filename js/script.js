a71044856aa610acd1f1cea0d815571e141abf43
charles-tech <charles.fernando.miranda@gmail.com>
2023-03-12 23:53:59 -0300

    Consultar-Cep

---
+++ js/script.js
@@ -0,0 +1,28 @@
+async function buscarEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+  try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCepConvertida = await consultaCep.json();
+    if (consultaCepConvertida.erro) {
+      throw Error("Cep não existente");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+    var bairro = document.getElementById("bairro");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+    bairro.value = consultaCepConvertida.bairro;
+
+    console.log(consultaCepConvertida);
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p> CEP inválido. Tente novamente </p>`;
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscarEndereco(cep.value));
